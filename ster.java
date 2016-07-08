1eb483311419038886a08347b7b5c569f7a011bb
sakthiprasannaa <sakthiprasannaa007@gmail.com>
2016-07-08 14:47:10 +0530

    Add files via upload

---
+++ ster.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class ster
+{
+public static void main(String[]args)throws IOException
+{
+String s="ans";
+String reverse = new StringBuffer(s).reverse().toString();
+System.out.println(reverse);
+}
+}
